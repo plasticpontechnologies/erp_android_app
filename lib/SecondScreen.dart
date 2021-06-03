e4c3c589fae5f6e362c34b7403537cb5b63017a6
plasticpontechnologies <plasticpontechnologies@gmail.com>
2021-06-03 11:31:41 +0530

    adding pages

--- lib/SecondScreen.dart
+++ lib/SecondScreen.dart
@@ -15,3 +15,20 @@ class _SecondScreenState extends State<SecondScreen> {
     );
   }
 }
+//
+//
+// class SecondScreen extends StatefulWidget {
+//   SecondScreen({Key key}) : super(key: key);
+//
+//   @override
+//   _SecondScreenState createState() => _SecondScreenState();
+// }
+//
+// class _SecondScreenState extends State<SecondScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: Text('sdfhjshdfj'),
+//     );
+//   }
+// }
--- lib/TABS/ChatScreen.dart
+++ lib/TABS/ChatScreen.dart
@@ -1,106 +1,105 @@
-import 'package:flutter/material.dart';
-
-import 'MainTabScreen.dart';
-
-class ChatScreen extends StatefulWidget {
-  const ChatScreen({Key key}) : super(key: key);
-
-  @override
-  _ChatScreenState createState() => _ChatScreenState();
-}
-
-class _ChatScreenState extends State<ChatScreen> {
-  @override
-  Widget build(BuildContext context) {
-
-    return DefaultTabController(
-      length: 4,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color.fromRGBO(222, 51, 21, 1.0),
-          bottom: TabBar(
-            tabs: [
-              Tab(
-                text: 'CHARTS New',
-              ),
-              Tab(
-                text: 'STAFF',
-              ),
-              Tab(
-                text: 'STUDENT',
-              ),
-              Tab(
-                text: 'SECTION',
-              ),
-
-    return 
-    DefaultTabController(
-        length: 4,
-        child: Scaffold(
-          appBar: AppBar(backgroundColor: Color.fromRGBO(222, 51, 21, 1.0),
-            bottom: TabBar(
-              tabs: [
-                
-                Tab(text: 'CHARTS',),
-                Tab(text: 'STAFF',),
-                Tab(text: 'STUDENT',),
-                Tab(text: 'SECTION',),
-     
-              ],
-              indicatorColor: Colors.white,
-            ),
-            title: Text('User Name'),
-            leading: IconButton(icon:Icon(Icons.arrow_back,//color:AppConstant.colorIcon,
-    ),
-    onPressed:()
-        {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => MaintabScreen(selectedIndexValue: 0,)),
-          );
-        }
-    //  automaticallyImplyLeading: false,
-    ),
-          ),
-          body: TabBarView(
-            children: [
-
-              Container(height:400,color: Colors.orange),
-
-              Container(height:400,color: Colors.green),
-              Container(height:400,color: Colors.yellow),
-              Container(height:400,color: Color.fromRGBO(250, 220, 175, 1.0)),
-
->>>>>>> ee445f14140ba1b6458fc7643039452d859fa48e
-            ],
-            indicatorColor: Colors.white,
-          ),
-          title: Text('User Name'),
-          leading: IconButton(
-              icon: Icon(
-                Icons.arrow_back, //color:AppConstant.colorIcon,
-              ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => MaintabScreen(
-                            selectedIndexValue: 0,
-                          )),
-                );
-              }
-              //  automaticallyImplyLeading: false,
-              ),
-        ),
-        body: TabBarView(
-          children: [
-            Container(height: 400, color: Colors.orange),
-            Container(height: 400, color: Colors.green),
-            Container(height: 400, color: Colors.yellow),
-            Container(height: 400, color: Color.fromRGBO(250, 220, 175, 1.0)),
-          ],
-        ),
-      ),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+//
+// import 'MainTabScreen.dart';
+//
+// class ChatScreen extends StatefulWidget {
+//   const ChatScreen({Key key}) : super(key: key);
+//
+//   @override
+//   _ChatScreenState createState() => _ChatScreenState();
+// }
+//
+// class _ChatScreenState extends State<ChatScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//
+//     return DefaultTabController(
+//       length: 4,
+//       child: Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: Color.fromRGBO(222, 51, 21, 1.0),
+//           bottom: TabBar(
+//             tabs: [
+//               Tab(
+//                 text: 'CHARTS New',
+//               ),
+//               Tab(
+//                 text: 'STAFF',
+//               ),
+//               Tab(
+//                 text: 'STUDENT',
+//               ),
+//               Tab(
+//                 text: 'SECTION',
+//               ),
+//
+//     return
+//     DefaultTabController(
+//         length: 4,
+//         child: Scaffold(
+//           appBar: AppBar(backgroundColor: Color.fromRGBO(222, 51, 21, 1.0),
+//             bottom: TabBar(
+//               tabs: [
+//
+//                 Tab(text: 'CHARTS',),
+//                 Tab(text: 'STAFF',),
+//                 Tab(text: 'STUDENT',),
+//                 Tab(text: 'SECTION',),
+//
+//               ],
+//               indicatorColor: Colors.white,
+//             ),
+//             title: Text('User Name'),
+//             leading: IconButton(icon:Icon(Icons.arrow_back,//color:AppConstant.colorIcon,
+//     ),
+//     onPressed:()
+//         {
+//           Navigator.push(
+//             context,
+//             MaterialPageRoute(builder: (context) => MaintabScreen(selectedIndexValue: 0,)),
+//           );
+//         }
+//     //  automaticallyImplyLeading: false,
+//     ),
+//           ),
+//           body: TabBarView(
+//             children: [
+//
+//               Container(height:400,color: Colors.orange),
+//
+//               Container(height:400,color: Colors.green),
+//               Container(height:400,color: Colors.yellow),
+//               Container(height:400,color: Color.fromRGBO(250, 220, 175, 1.0)),
+//
+//             ],
+//             indicatorColor: Colors.white,
+//           ),
+//           title: Text('User Name'),
+//           leading: IconButton(
+//               icon: Icon(
+//                 Icons.arrow_back, //color:AppConstant.colorIcon,
+//               ),
+//               onPressed: () {
+//                 Navigator.push(
+//                   context,
+//                   MaterialPageRoute(
+//                       builder: (context) => MaintabScreen(
+//                             selectedIndexValue: 0,
+//                           )),
+//                 );
+//               }
+//               //  automaticallyImplyLeading: false,
+//               ),
+//         ),
+//         body: TabBarView(
+//           children: [
+//             Container(height: 400, color: Colors.orange),
+//             Container(height: 400, color: Colors.green),
+//             Container(height: 400, color: Colors.yellow),
+//             Container(height: 400, color: Color.fromRGBO(250, 220, 175, 1.0)),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
