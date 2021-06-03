2bc81668c01198ca6363fd7eaafccfb2ac06664b
plasticpontechnologies <plasticontechnologies@gmail.com>
2021-06-03 07:40:30 +0530

    first commit
    Signed-off-by: plasticpontechnologies <plasticontechnologies@gmail.com>

--- lib/TABS/ChatScreen.dart
+++ lib/TABS/ChatScreen.dart
@@ -12,47 +12,54 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
-    return 
-    DefaultTabController(
-        length: 4,
-        child: Scaffold(
-          appBar: AppBar(backgroundColor: Color.fromRGBO(222, 51, 21, 1.0),
-            bottom: TabBar(
-              tabs: [
-                Tab(text: 'CHARTS',),
-                Tab(text: 'STAFF',),
-                Tab(text: 'STUDENT',),
-                Tab(text: 'SECTION',),
-     
-              ],
-              indicatorColor: Colors.white,
-            ),
-            title: Text('User Name'),
-            leading: IconButton(icon:Icon(Icons.arrow_back,//color:AppConstant.colorIcon,
-    ),
-    onPressed:()
-        {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => MaintabScreen(selectedIndexValue: 0,)),
-          );
-        }
-    //  automaticallyImplyLeading: false,
-    ),
-          ),
-          body: TabBarView(
-            children: [
-
-              Container(height:400,color: Colors.orange),
-
-              Container(height:400,color: Colors.green),
-              Container(height:400,color: Colors.yellow),
-              Container(height:400,color: Color.fromRGBO(250, 220, 175, 1.0)),
-
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromRGBO(222, 51, 21, 1.0),
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                text: 'CHARTS New',
+              ),
+              Tab(
+                text: 'STAFF',
+              ),
+              Tab(
+                text: 'STUDENT',
+              ),
+              Tab(
+                text: 'SECTION',
+              ),
             ],
+            indicatorColor: Colors.white,
           ),
+          title: Text('User Name'),
+          leading: IconButton(
+              icon: Icon(
+                Icons.arrow_back, //color:AppConstant.colorIcon,
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => MaintabScreen(
+                            selectedIndexValue: 0,
+                          )),
+                );
+              }
+              //  automaticallyImplyLeading: false,
+              ),
         ),
-      
+        body: TabBarView(
+          children: [
+            Container(height: 400, color: Colors.orange),
+            Container(height: 400, color: Colors.green),
+            Container(height: 400, color: Colors.yellow),
+            Container(height: 400, color: Color.fromRGBO(250, 220, 175, 1.0)),
+          ],
+        ),
+      ),
     );
   }
 }
