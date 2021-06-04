907659da9a5f5e4dfe1df4cfda9016ad7d17801e
Venkat <kvr256@gmail.com>
2021-06-04 08:04:08 +0530

    Chat Screen and School Info Screen

--- lib/ChatScreen.dart
+++ lib/ChatScreen.dart
@@ -13,19 +13,126 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Chat Screenn'),
-        leading: IconButton(icon:Icon(Icons.arrow_back,//color:AppConstant.colorIcon,
-        ),
-            onPressed:()
-            {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => MaintabScreen(selectedIndexValue: 0,)),
-              );
+    return DefaultTabController
+    (
+      length: 4,
+      child: Scaffold(appBar: AppBar(title: Text('Chat Screenn'),
+      backgroundColor: Colors.deepOrange,
+          leading: IconButton(icon:Icon(Icons.arrow_back,//color:AppConstant.colorIcon,
+          ),
+              onPressed:()
+              {
+                
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MaintabScreen(selectedIndexValue: 0,)),
+                );
+    
+              }
+            //  automaticallyImplyLeading: false,
+          ),
+           bottom: TabBar(
+              tabs: [
+                Tab(text:"CHARTS"),
+                Tab(text:"STAFF"),
+                Tab(text:"STUDENT"),
+                                Tab(text:"SECTION"),
 
-            }
-          //  automaticallyImplyLeading: false,
-        )));
+              ],indicatorColor: Colors.white,
+            ),),
+
+body: TabBarView(
+            children: [
+
+Container(
+        color: Color.fromRGBO(250, 220, 175, 1.0),
+
+  child:   new ListView.builder
+    (
+      itemCount: 5,
+      itemBuilder: (BuildContext ctxt, int index) {
+       return Padding(
+         padding: const EdgeInsets.all(2.0),
+         child: Card(
+           child: ListTile(
+             leading: Icon(Icons.directions_car),
+             title: Text('User Name'),
+             subtitle: Text('Sub Name'),
+           ),
+         ),
+       );
+      }
+    ),
+),
+  
+Container(
+        color: Color.fromRGBO(250, 220, 175, 1.0),
+
+  child:   new ListView.builder
+    (
+      itemCount: 5,
+      itemBuilder: (BuildContext ctxt, int index) {
+       return Padding(
+         padding: const EdgeInsets.all(2.0),
+         child: Card(
+           child: ListTile(
+             leading: Icon(Icons.directions_car),
+             title: Text('User Name'),
+             subtitle: Text('Sub Name'),
+           ),
+         ),
+       );
+      }
+    ),
+),
+
+Container(
+        color: Color.fromRGBO(250, 220, 175, 1.0),
+
+  child:   new ListView.builder
+    (
+      itemCount: 5,
+      itemBuilder: (BuildContext ctxt, int index) {
+       return Padding(
+         padding: const EdgeInsets.all(2.0),
+         child: Card(
+           child: ListTile(
+             leading: Icon(Icons.directions_car),
+             title: Text('User Name'),
+             subtitle: Text('Sub Name'),
+           ),
+         ),
+       );
+      }
+    ),
+),
+
+Container(
+        color: Color.fromRGBO(250, 220, 175, 1.0),
+
+  child:   new ListView.builder
+    (
+      itemCount: 5,
+      itemBuilder: (BuildContext ctxt, int index) {
+       return Padding(
+         padding: const EdgeInsets.all(2.0),
+         child: Card(
+           child: ListTile(
+             leading: Icon(Icons.directions_car),
+             title: Text('User Name'),
+             subtitle: Text('Sub Name'),
+           ),
+         ),
+       );
+      }
+    ),
+),
+
+            ],
+          ),
+          ),
+          
+    );
 
   }
 }
--- lib/TABS/SchoolInformation.dart
+++ lib/TABS/SchoolInformation.dart
@@ -1,6 +1,6 @@
 
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 // import 'TABS/SchoolInformation.dart';
 
@@ -30,257 +30,70 @@ class _SchoolInformationScreenState extends State<SchoolInformationScreen> {
     ),
 
       body:
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
+Container(height: screenSize.height,
+               child: GridView.count(
 
-          children: [
+          // Create a grid with 2 columns. If you change the scrollDirection to
+          // horizontal, this produces 2 rows.
+          crossAxisCount: 2,
 
-            Expanded(
-              child:
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-
-                children: [
-
-                  Expanded(child:
-                  Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    child:
-
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-
-                      children: [
+          // Generate 100 widgets that display their index in the List.
+          children: List.generate(3, (index) {
 
 
-                        Image.asset('assets/s2.jpeg',width: screenSize.width/4,
-                          height: 90,),
-                        SizedBox(height:12),
-                        Text('Basic Details',style: TextStyle(color:  Color.fromRGBO(221, 51, 21, 1.0),
-                            fontFamily: 'Roboto',fontWeight: FontWeight.w500,
-                            fontSize: 20),),
+            String strImageName = '';
+
+            if(index == 0)
+              {
+                strImageName = 'assets/logo.png';
+              }
+            if(index == 0)
+            {
+              strImageName = 'assets/logo.png';
+            }
+            if(index == 0)
+            {
+              strImageName = 'assets/logo.png';
+            }
+            return Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: Card(
+
+                  color: Color.fromRGBO(255, 255, 255, 1.0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(0.0),
+                    child: Center(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children:
+                          [
+                            index == 0 ?
+                            SizedBox(child: ClipRRect(
+                              borderRadius: BorderRadius.circular(25.0),
+                              child: Image.asset('assets/logo.png')),height: 50,) :
+                            SizedBox(child: ClipRRect(
+                                borderRadius: BorderRadius.circular(25.0),
+                                child: Image.asset('assets/logo.png')),height: 50,) ,
 
 
-                      ],),
+                          SizedBox(height: 4,),
 
+                          Text('Class',style: TextStyle(color: Colors.orange),)
 
-                    color:Colors.white,
-
-                  ),
-
-                  ),
-
-                  SizedBox(width:20),
-
-
-                  Expanded(child:
-                  Card(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-
-
-                        Image.asset('assets/s3.jpeg',width: screenSize.width/4,
-                          height: 90,),
-                        SizedBox(height:12),
-
-                        Text('logo & Social',style: TextStyle(color:  Color.fromRGBO(221, 51, 21, 1.0),
-                            fontFamily: 'Roboto',fontWeight: FontWeight.w500,
-                            fontSize: 20),),
-
-
-                      ],),
-                    // color: Color(0xFF1D1E33),
-                    color:Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
+                        ],)
                     ),
                   ),
-
-                  ),
-
-
-
-
-                ],),
-
-
-            ),
-
-
-
-
-            SizedBox(height:12),
-
-            // Expanded(child: Card(
-            //   child: Column(
-            //     crossAxisAlignment: CrossAxisAlignment.center,
-            //     mainAxisAlignment: MainAxisAlignment.center,
-            //     children: [
-            //
-            //       // Text('HEIGHT',style: TextStyle(color: Colors.white,
-            //       //     fontFamily: 'Roboto',fontWeight: FontWeight.w500,
-            //       //     fontSize: 20),),
-            //       SizedBox(height: 4,),
-            //       Text(_currentSliderValue.toString(),style: TextStyle(color: Colors.white,
-            //           fontFamily: 'Roboto',fontWeight: FontWeight.w500,
-            //           fontSize: 20),),
-            //       SizedBox(height: 4,),
-            //
-            //       Slider(
-            //         activeColor: Color(0xFFEB1555),
-            //         inactiveColor: Colors.grey,
-            //         value: _currentSliderValue,
-            //         min: 0,
-            //         max: 100,
-            //         divisions: 10,
-            //         label: _currentSliderValue.round().toString(),
-            //         onChanged: (double value) {
-            //           setState(() {
-            //
-            //             _currentSliderValue = value;
-            //
-            //             print(value);
-            //
-            //           });
-            //         },
-            //       ),
-            //
-            //
-            //
-            //
-            //     ],),
-            //   color: Color(0xFF1D1E33),
-            //   shape: RoundedRectangleBorder(
-            //     borderRadius: BorderRadius.circular(8.0),
-            //   ),
-            // )),
-
-            SizedBox(height:12),
-
-            Expanded(
-              child:
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-
-                children: [
-
-                  Expanded(child:
-                  Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    child:
-
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-
-                      children: [
-
-
-                        Image.asset('assets/s1.jpeg',width: screenSize.width/4,
-                          height: 120,),
-                        SizedBox(height:12),
-                        Text('Academic Session',style: TextStyle(color:  Color.fromRGBO(221, 51, 21, 1.0),
-                            fontFamily: 'Roboto',fontWeight: FontWeight.w500,
-                            fontSize: 20),),
-
-
-                      ],),
-
-
-                    color:Colors.white,
-
-                  ),
-
-                  ),
-
-                  SizedBox(width:20),
-
-
-                  Expanded(child:
-                  Card(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-
-                        Image.asset('assets/logo.png',width: screenSize.width/4,
-                          height: 120,),
-
-                        SizedBox(height:12),
-
-                        Text('logo & Social',style: TextStyle(color:  Color.fromRGBO(221, 51, 21, 1.0),
-                            fontFamily: 'Roboto',fontWeight: FontWeight.w500,
-                            fontSize: 20),),
-
-
-                      ],),
-                    // color: Color(0xFF1D1E33),
-                    color:Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                  ),
-
-                  ),
-
-
-
-
-                ],),
-
-
-            ),
-
-            SizedBox(height:12),
-
-            SizedBox(width: screenSize.width,
-              height: 70,
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor:  MaterialStateProperty.all<Color>(Color(
-                      0xFFEB1555)),
-
                 ),
-                child:
-                Text('CALCULATE',
-
-                  style: TextStyle(color: Colors.white,
-                    letterSpacing: 1.25,
-                    fontSize: 20,),
-                ),
-
-                onPressed: ()
-                {
-
-
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => SchoolInformationScreen(),),);
+              
+            );
+          }),
+        ),
+             ),
 
 
 
-                },
-
-              ),
-            ),
-
-          ],),
-      ),
-
-
-
-
-      backgroundColor: Color.fromRGBO(22, 25, 41, 1.0),
+      backgroundColor: Color.fromRGBO(250, 220, 175, 1.0),
 
     );
 
